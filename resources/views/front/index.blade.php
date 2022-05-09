a0f055a67cffe5bed20c234befef97548a5289c7
Samy <samypulat@gmail.com>
2022-05-09 10:50:09 +0200

    Intégration du template

---
+++ resources/views/front/index.blade.php
@@ -0,0 +1,11 @@
+@extends('front.layouts.app')
+
+@section('content')
+    @include('front.partials.header')
+    @include('front.partials.modal')
+    @include('front.partials.banner')
+    @include('front.partials.services')
+    @include('front.partials.about')
+    @include('front.partials.clients')
+    @include('front.partials.newsletter')
+@endsection
